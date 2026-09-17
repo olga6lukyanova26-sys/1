a60da1077a17fc09eb0664eb5b2f1b37af5c5bb2
olya <olga6lukyanova26@gmail.com>
2026-09-18 01:40:44 +0500

    Create main.go

---
+++ golang/ch1/Dup1/main.go
@@ -0,0 +1,23 @@
+//Dupl выводит текст каждой строки, которая появляется в cтандартном вводе более одного раза, а также количество ее появлений.
+
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "os"
+)
+
+func main() {
+  counts := make(map[string]int)
+  input := bufio.NewScanner(os.Stdin)
+  for input.Scan() {
+    counts[input.Text()]++
+}
+//Примечание: игнорируем потенциальные ошибки из input.Err()
+for line, n := range counts { 
+  if n > 1 {
+    fmt.Printf("%d\t%s\n", n, line)
+    }
+  }
+} 
